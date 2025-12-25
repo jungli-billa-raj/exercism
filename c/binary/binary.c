0bd5386363db3adc56f7ff75f2fe71d369cccf9e
Kumar Raj <rajbhkseries@gmail.com>
2025-12-25 12:44:34 +0530

    finished binary

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -3,5 +3,22 @@
 
 
 int convert(const char *input){
-  
+  int length = 0;
+  int no = 0;
+  int power = 0;
+  for (int i=0;input[i]!='\0';i++) {
+    if (input[i]!='1' && input[i]!='0'){
+      return INVALID;
+    }
+    ++length;
+  }
+  for (int i=length-1;i>=0;i--){
+    if (input[i]=='0'){
+      power++;
+      continue;
+    }
+    no+=pow(2,power);
+    power++;
+  }
+  return no;
 }
