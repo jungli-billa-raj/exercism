60cb2a27e9040038b6cc49d18d257a1e2c3276dd
Kumar Raj <rajbhkseries@gmail.com>
2026-02-28 16:31:48 +0530

    finishsed high-scores

--- zig/high-scores/high_scores.zig
+++ zig/high-scores/high_scores.zig
@@ -1,45 +1,33 @@
 const std = @import("std");
 
 pub const HighScores = struct {
-    // This struct, as well as its fields and methods, needs to be implemented.
-
     scores: []const i32,
-    top3: []const i32,
+    top3: [3]i32,
+    top3_len: usize,
 
     pub fn init(scores: []const i32) HighScores {
-        // sort
-        // var t:[3]i32 = .{-1} ** 3;
-        var t:[3]i32 = .{-1, -1, -1};
-        // var t = &[_]i32{-1, -1, -1};
+       var hs = HighScores{
+           .scores = scores,
+           .top3 = .{ -1, -1, -1 },
+           .top3_len = @min(3, scores.len),
+       };
 
-        t[0] = -1;
-        t[1] = -1;
-        t[2] = -1;
-
-        for (scores) |v| {
-            if (v>t[0]){
-                t[2] = t[1];
-                t[1] = t[0];
-                t[0] =v;
-            } else if (v>t[1]){
-                t[2] = t[1];
-                t[1] = v;
-            } else if (v>t[2]){
-                t[2] =v;
+       // finding top 3
+       for (scores) |v| {
+            if (v>hs.top3[0]){
+                hs.top3[2] = hs.top3[1];
+                hs.top3[1] = hs.top3[0];
+                hs.top3[0] = v; 
+            } else if (v>hs.top3[1]){
+                hs.top3[2] = hs.top3[1];
+                hs.top3[1] = v;
+            } else if (v>hs.top3[2]){
+                hs.top3[2] = v;
             }
-        }
+       }
 
-        const n = @min(3, scores.len);
+       return hs;
 
-        std.debug.print("{any}\n\n", .{t[0..n]});
-        const hs = HighScores{
-            .scores = scores,
-            // .top3 = &(.{0} ** 3),
-            .top3 = t[0..n],
-        };
-
-
-        return hs;
     }
 
     pub fn latest(self: *const HighScores) ?i32 {
@@ -55,36 +43,7 @@ pub const HighScores = struct {
     }
 
     pub fn personalTopThree(self: *const HighScores) []const i32 {
-      // var first:i32 = -1;
-      // var second:i32 = -1;
-      // var third:i32 = -1;
-      //
-      // for (self.scores) |v|{
-      //   if (v>first){
-      //       third = second;
-      //       second = first;
-      //       first = v;
-      //   } else if (v>second) {
-      //       third = second;
-      //       second = v;
-      //   } else if (v>third){
-      //       third = v;
-      //   }
-      // }
-      // self.top3 =  .{ first, second, third };
-      //
-      // return self.top3[0..];
-
-        return self.top3[0..];
+        return self.top3[0..self.top3_len];
     }
 };
 
-//  [ 11, 22, 12, 33, 56, 99, 68, 60 , 32]
-pub fn main() !void {
-    const scores = &[_]i32{ 40, 100, 70 };
-    const my_high_score =  HighScores.init(scores);
-    std.debug.print("{any} \n\n", .{my_high_score.latest()});
-    std.debug.print("{any} \n\n", .{my_high_score.personalBest()});
-    std.debug.print("{any} \n\n", .{my_high_score.personalTopThree()});
-    std.debug.print("{any} \n\n", .{my_high_score.scores});
-}
