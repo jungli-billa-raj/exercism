6df08721a1e38c3d0ca0cc5e14d7fb2049e3aae5
Kumar Raj <rajbhkseries@gmail.com>
2026-02-28 16:45:02 +0530

    finished squre-root

--- zig/square-root/square_root.zig
+++ zig/square-root/square_root.zig
@@ -1,18 +1,20 @@
 pub fn squareRoot(radicand: usize) usize {
     // Newton's formula
-    // var x:f32 = 0;
-    //
-    // while (true) {
-    //     if (@as(usize,@floor(x*x)) == radicand){
-    //         break;
-    //     }
-    //     x = 1/2*(x + @as(f32,@floatFromInt(radicand))/x);
-    // }
-    //
-    // return @as(usize, @intFromFloat(x)); 
-    const x:usize = 1;
-    for (0..radicand) |i| {
-        if (i*i == radicand) return i;
+    var x:f32 = 10;
+
+    while (true) {
+        const rounded = @round(x*x);
+        const rounded_usize:usize = @intFromFloat(rounded);
+        if (rounded_usize == radicand){
+            break;
+        }
+        x = 0.5*(x + @as(f32, @floatFromInt(radicand))/x);
     }
-    return x;
+
+    return @as(usize, @intFromFloat(x)); 
+    // const x:usize = 1;
+    // for (0..radicand) |i| {
+    //     if (i*i == radicand) return i;
+    // }
+    // return x;
 }
