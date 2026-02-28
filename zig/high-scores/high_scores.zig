a45b1bb94e86480b65fa90e4af8a337eddd4d382
Kumar Raj <rajbhkseries@gmail.com>
2026-02-28 14:58:03 +0530

    dimag ka bharosa ho gya h bro

--- zig/high-scores/high_scores.zig
+++ zig/high-scores/high_scores.zig
@@ -4,23 +4,42 @@ pub const HighScores = struct {
     // This struct, as well as its fields and methods, needs to be implemented.
 
     scores: []const i32,
-    sorted: [100]i32,
+    top3: []const i32,
 
     pub fn init(scores: []const i32) HighScores {
-        // sort 
-        var hs = HighScores{
-            .scores = scores,
-            .sorted = .{0} ** 100,
-        };
+        // sort
+        // var t:[3]i32 = .{-1} ** 3;
+        var t:[3]i32 = .{-1, -1, -1};
+        // var t = &[_]i32{-1, -1, -1};
 
-        //copy of scores in sorted[]
-        for (scores, 0..) |v,i| {
-            hs.sorted[i] = v;
+        t[0] = -1;
+        t[1] = -1;
+        t[2] = -1;
+
+        for (scores) |v| {
+            if (v>t[0]){
+                t[2] = t[1];
+                t[1] = t[0];
+                t[0] =v;
+            } else if (v>t[1]){
+                t[2] = t[1];
+                t[1] = v;
+            } else if (v>t[2]){
+                t[2] =v;
+            }
         }
 
-        std.sort.block(i32, &hs.sorted, {}, comptime std.sort.desc(i32));
+        const n = @min(3, scores.len);
 
-        return .{.scores = scores, .sorted = hs.sorted };
+        std.debug.print("{any}\n\n", .{t[0..n]});
+        const hs = HighScores{
+            .scores = scores,
+            // .top3 = &(.{0} ** 3),
+            .top3 = t[0..n],
+        };
+
+
+        return hs;
     }
 
     pub fn latest(self: *const HighScores) ?i32 {
@@ -56,9 +75,16 @@ pub const HighScores = struct {
       //
       // return self.top3[0..];
 
-    const n = @min(3, self.scores.len);
-    return self.sorted[0..n];
+        return self.top3[0..];
     }
 };
 
 //  [ 11, 22, 12, 33, 56, 99, 68, 60 , 32]
+pub fn main() !void {
+    const scores = &[_]i32{ 40, 100, 70 };
+    const my_high_score =  HighScores.init(scores);
+    std.debug.print("{any} \n\n", .{my_high_score.latest()});
+    std.debug.print("{any} \n\n", .{my_high_score.personalBest()});
+    std.debug.print("{any} \n\n", .{my_high_score.personalTopThree()});
+    std.debug.print("{any} \n\n", .{my_high_score.scores});
+}
